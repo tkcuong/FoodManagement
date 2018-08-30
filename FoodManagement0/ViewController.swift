f79e04bd6db2ddf25e5f91707c38b26067f38828
Cuong Tieu-Kim <tkcuong.fee.hut@gmail.com>
2018-08-30 10:30:25 +0700

    Initial Commit

---
+++ FoodManagement0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodManagement0
+//
+//  Created by Cuong Tieu-Kim on 8/30/18.
+//  Copyright © 2018 TDC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
